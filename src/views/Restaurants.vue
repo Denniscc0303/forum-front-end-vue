3567d62bd04b77dedb3876fc931127f1b61a5c33
Denniscc <tonynooy@gmail.com>
2021-04-11 21:55:22 +0800

    3_A20 User Profile

---
+++ src/views/Restaurants.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container py-5">
+    <NavTabs />
+    <!-- 餐廳類別標籤 RestaurantsNavPills -->
+
+    <div class="row">
+      <!-- 餐廳卡片 RestaurantCard-->
+      <RestaurantCard />
+    </div>
+
+    <!-- 分頁標籤 RestaurantPagination -->
+  </div>
+</template>
+
+<script>
+import NavTabs from "./../components/NavTabs";
+import RestaurantCard from "./../components/RestaurantCard";
+export default {
+  components: {
+    NavTabs,
+    RestaurantCard,
+  },
+};
+</script>
